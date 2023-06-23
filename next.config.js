cdf82aa0135247be74f55de3a5561349362d01c3
39sho <g.39sho@gmail.com>
2023-06-23 10:29:29 +0900

    first prototype

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
 const nextConfig = {
     output: 'export',
 }
-=======
-const nextConfig = {}
->>>>>>> cfa47344b785b30e595f923784e3fca2a4c65dcf
 
 module.exports = nextConfig
